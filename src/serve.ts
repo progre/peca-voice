e87ce634ef1c15556ab2e0f97e96da93a795f4c8
progre <djyayutto@gmail.com>
2017-11-26 11:08:57 +0900

    style: fix style

--- src/serve.ts
+++ src/serve.ts
@@ -1,57 +1,57 @@
-try { require("source-map-support").install(); } catch (e) { /* empty */ }
-import * as http from "http";
-import * as fs from "fs";
-const Gitter = require("node-gitter");
-import { postStatus } from "./gitterinterface";
-import { getLatests, getSince } from "./twitterinterface";
-import { configure, getLogger } from "log4js";
+import * as fs from 'fs';
+import * as http from 'http';
+// tslint:disable-next-line:variable-name
+const Gitter = require('node-gitter');
+import { postStatus } from './gitterinterface';
+import { getLatests, getSince } from './twitterinterface';
+const { configure, getLogger } = require('log4js');
 configure({
-    appenders: [{ type: "console", layout: { type: "basic" } }]
+  appenders: [{ type: 'console', layout: { type: 'basic' } }],
 });
-let logger = getLogger();
+const logger = getLogger();
 
 if (process.env.GITTER_ROOM == null
-    || process.env.GITTER_TOKEN == null
-    || process.env.TWITTER_CONSUMER_TOKEN == null
-    || process.env.TWITTER_CONSUMER_SECRET == null
-    || process.env.TWITTER_ACCESS_TOKEN_KEY == null
-    || process.env.TWITTER_ACCESS_TOKEN_SECRET == null) {
-    logger.fatal("Tokens not found.");
-    process.exit(1);
+  || process.env.GITTER_TOKEN == null
+  || process.env.TWITTER_CONSUMER_TOKEN == null
+  || process.env.TWITTER_CONSUMER_SECRET == null
+  || process.env.TWITTER_ACCESS_TOKEN_KEY == null
+  || process.env.TWITTER_ACCESS_TOKEN_SECRET == null) {
+  logger.fatal('Tokens not found.');
+  process.exit(1);
 }
 
-http.createServer((req, res) => {
-    const gitterToken = process.env.GITTER_TOKEN;
-    const twitterTokens = {
-        consumerKey: process.env.TWITTER_CONSUMER_TOKEN,
-        consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-        accessTokenKey: process.env.TWITTER_ACCESS_TOKEN_KEY,
-        accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    };
-    let roomPath = process.env.GITTER_ROOM;
-    fs.readFile("/tmp/maxId", "utf8", async (err, data) => {
-        if (err) {
-            if (err.code === "ENOENT") {
-                const { maxId, since } = await getSince(twitterTokens);
-                fs.writeFile("/tmp/maxId", maxId);
-                let gitter = new Gitter(gitterToken);
-                let room = await gitter.rooms.join(roomPath);
-                logger.info(await room.send(`Since ${since}.`));
-                res.statusCode = 200;
-                res.end();
-                return;
-            }
-            res.statusCode = 500;
-            res.end(JSON.stringify(err));
-            return;
-        }
-        const { maxId, statuses } = await getLatests(twitterTokens, data);
-        statuses.forEach((status) => {
-            logger.info("Found. " + JSON.stringify(status));
-            postStatus(status, gitterToken, roomPath);
-        });
-        fs.writeFile("/tmp/maxId", maxId);
+http.createServer((_, res) => {
+  const gitterToken = process.env.GITTER_TOKEN!;
+  const twitterTokens = {
+    consumerKey: process.env.TWITTER_CONSUMER_TOKEN,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessTokenKey: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  };
+  const roomPath = process.env.GITTER_ROOM!;
+  fs.readFile('/tmp/maxId', 'utf8', async (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        const { maxId: maxIdOnFirstTime, since } = await getSince(twitterTokens);
+        fs.writeFile('/tmp/maxId', maxIdOnFirstTime, (err1) => { console.error(err1); });
+        const gitter = new Gitter(gitterToken);
+        const room = await gitter.rooms.join(roomPath);
+        logger.info(await room.send(`Since ${since}.`));
         res.statusCode = 200;
         res.end();
+        return;
+      }
+      res.statusCode = 500;
+      res.end(JSON.stringify(err));
+      return;
+    }
+    const { maxId, statuses } = await getLatests(twitterTokens, data);
+    statuses.forEach((status) => {
+      logger.info(`Found. ${JSON.stringify(status)}`);
+      postStatus(status, gitterToken, roomPath).catch((e) => { console.error(e); });
     });
+    fs.writeFile('/tmp/maxId', maxId, (e) => { console.error(e); });
+    res.statusCode = 200;
+    res.end();
+  });
 }).listen(3000);
--- src/twitterinterface.ts
+++ src/twitterinterface.ts
@@ -1,20 +1,21 @@
-const jaco = require("jaco");
-const Twitter = require("twitter");
+const jaco = require('jaco');
+// tslint:disable-next-line:variable-name
+const Twitter = require('twitter');
 
 const HIRAGANA_KEYWORDS = [
-  "ぴあきゃす",
-  "ぴあきゃすと",
-  "ぴあかす",
-  "ぺかすて",
-  "ぺか界隈"
+  'ぴあきゃす',
+  'ぴあきゃすと',
+  'ぴあかす',
+  'ぺかすて',
+  'ぺか界隈',
 ];
 const KATAKANA_KEYWORDS = HIRAGANA_KEYWORDS.map(x => jaco.katakanize(x));
 export const ALPHABET_KEYWORDS = [
-  "peercast",
-  "peercaststation",
-  "pecast",
-  "pecastarter",
-  "peca界隈"
+  'peercast',
+  'peercaststation',
+  'pecast',
+  'pecastarter',
+  'peca界隈',
 ];
 const SEARCH_KEYWORDS = ALPHABET_KEYWORDS
   .concat(HIRAGANA_KEYWORDS.map(x => `"${x}"`))
@@ -24,41 +25,41 @@ export const BOLD_KEYWORDS = ALPHABET_KEYWORDS
   .concat(KATAKANA_KEYWORDS);
 
 const SCREEN_NAME_BLACK_LIST = [
-  "aquari_bot",
-  "bestyasuhirojp",
-  "inatami_bot",
-  "sapioshan",
-  "skc_anniversary",
-  "SKC_nmcm_bot",
-  "Takayasikibot",
-  "tks_kool_bot",
-  "yukkuri_livech"
+  'aquari_bot',
+  'bestyasuhirojp',
+  'inatami_bot',
+  'sapioshan',
+  'skc_anniversary',
+  'SKC_nmcm_bot',
+  'Takayasikibot',
+  'tks_kool_bot',
+  'yukkuri_livech',
 ];
 
-const PECASTARTER_CONSTANT_MESSAGE = "PeerCastで配信中！";
+const PECASTARTER_CONSTANT_MESSAGE = 'PeerCastで配信中！';
 
 export async function getSince(tokens: any) {
-  let client = new Twitter({
+  const client = new Twitter({
     consumer_key: tokens.consumerKey,
     consumer_secret: tokens.consumerSecret,
     access_token_key: tokens.accessTokenKey,
-    access_token_secret: tokens.accessTokenSecret
+    access_token_secret: tokens.accessTokenSecret,
   });
-  let latest = await getLatest(client);
+  const latest = await getLatest(client);
   return {
     maxId: latest.id_str,
-    since: new Date(latest.created_at)
+    since: new Date(latest.created_at),
   };
 }
 
 export async function getLatests(tokens: any, currentMaxId: string) {
-  let client = new Twitter({
+  const client = new Twitter({
     consumer_key: tokens.consumerKey,
     consumer_secret: tokens.consumerSecret,
     access_token_key: tokens.accessTokenKey,
-    access_token_secret: tokens.accessTokenSecret
+    access_token_secret: tokens.accessTokenSecret,
   });
-  let tweets = await getTweets(client, currentMaxId);
+  const tweets = await getTweets(client, currentMaxId);
   if (tweets.errors != null) {
     throw new Error(JSON.stringify(tweets));
   }
@@ -67,12 +68,12 @@ export async function getLatests(tokens: any, currentMaxId: string) {
     maxId,
     statuses: (<any[]>tweets.statuses)
       .reverse()
-      .filter(status => isValid(status))
+      .filter(isValid),
   };
 }
 
 function isValid(status: any) {
-  let screenName = status.user.screen_name;
+  const screenName = status.user.screen_name;
   if (SCREEN_NAME_BLACK_LIST.indexOf(screenName) >= 0) {
     return false;
   }
@@ -87,26 +88,26 @@ function isValid(status: any) {
 }
 
 async function getLatest(client: any) {
-  let params = {
-    q: SEARCH_KEYWORDS.join(" OR "),
-    count: 1
+  const params = {
+    q: SEARCH_KEYWORDS.join(' OR '),
+    count: 1,
   };
-  let tweets = await get(client, "search/tweets", params);
+  const tweets = await get(client, 'search/tweets', params);
   return tweets.statuses[0];
 }
 
-function getTweets(client: any, sinceId: string) {
-  let params = {
-    q: SEARCH_KEYWORDS.join(" OR "),
+async function getTweets(client: any, sinceId: string) {
+  const params = {
+    q: SEARCH_KEYWORDS.join(' OR '),
     count: 100,
-    since_id: sinceId
+    since_id: sinceId,
   };
-  return get(client, "search/tweets", params);
+  return get(client, 'search/tweets', params);
 }
 
-function get(client: any, path: string, params: any) {
+async function get(client: any, path: string, params: any) {
   return new Promise<any>((resolve, reject) => {
-    client.get(path, params, (e: any, tweets: any, response: any) => {
+    client.get(path, params, (e: any, tweets: any, _: any) => {
       if (e != null) {
         reject(e);
         return;
